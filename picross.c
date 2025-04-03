666dce19cad4de38c7450366020ce80d6a3b71d0
mistekko <steamlie@protonmail.com>
2025-04-03 08:02:06 -0500

    Update program structure
    I failed to plan very far ahead when beginning work on this, and this commit
    remedies that. Having a 'Row' struct to store all the details of a row makes
    if all you're working with is rows, but it's got some problems:
    
    - The 'Row' struct previously (before this commit) stores hints and number of
      hints (array) and the row's contents (ull mask); storing the content is
      unnecessary since it's only used to calculate the hints
    - If we're storing hints in structs, we'll need a struct for the columns, too,
      but it'd only store hints. Thus, I changed the 'Row' struct into a 'Hint'
      struct to be used for both Rows and Columns. To simplify the hint finding
      code, 'Hint' structs are linked lists
    
    I also added stubs for upcoming implementation and a 'Game' struct which stores
    the game's (global) state; both I did to further clarify the new structure.

--- picross.c
+++ picross.c
@@ -1,7 +1,38 @@
+/* Local Variables: */
+/* compile-command: "cc picross.c -Wall -Wextra -pedantic -o p" */
+/* End: */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-static inline unsigned long long int binStoint(const char *s)
+struct Hint;
+struct Hint {
+	int hint;
+	struct Hint* next;
+};
+
+struct Game {
+	int nrows;
+	int ncols;
+	struct Hint **rowhints;
+	struct Hint **colhints;
+	unsigned long long int *rowfilledmasks;
+	unsigned long long int *rowrejectmasks; /* crossed-out cells */
+	int posx, posy;
+};
+
+static inline unsigned long long int binStoint(const char *s);
+
+static void parselevel(); // todo
+static inline int rowlength(unsigned long long int row);
+static struct Hint *findrowhints(unsigned long long int row);
+static void printhints(struct Hint *first);
+static char cellstatus(int x, int y); // todo: need Game object first
+static void markcell(int x, int y, int reject); /* reject: 0=fill, 1=reject */ //todo
+static void creategame(); // todo: returns fully populated Game struct
+
+static unsigned long long int
+binStoint(const char *s)
 {
         unsigned long long int i = 0;
         while (*s) {
@@ -11,61 +42,67 @@ static inline unsigned long long int binStoint(const char *s)
         return i;
 }
 
-static inline int rowlength(unsigned long long int i)
+static inline int
+rowlength(unsigned long long int row)
 {
 	int length = 0;
-	while (i >> length) {
+	while (row >> length) {
 		length += 1;
 	}
 	return length - 1; /* first 1 allows for leading 0s in row content*/
 }
 
-struct Row {
-	int nhints;
-	int* hints;
-	unsigned long long int content;
-};
-
-struct Row * makerow(unsigned long long int content) {
-	int hints[rowlength(content)];
+static struct Hint
+*findrowhints(unsigned long long int row)
+{
+	int maxhints = rowlength(row) / 2 + 1;
+	int hints[maxhints];
 	int nhints = 0;
 	int chain = 0;
-	int count = 0;
-	while (count < rowlength(content)) {
-		if (content >> count & 1) {
+	for (int i = 0; i < rowlength(row); i++) {
+		if (row >> i & 1) {
 			if (!chain)
 				nhints++;
 			chain++;
 			hints[nhints-1] = chain;
 		} else
 			chain = 0;
-		count++;
 	}
+	if (!nhints)
+		exit(1);
 
-	struct Row *row = malloc (sizeof *row);
-	int *hintsREAL = malloc (sizeof (int[nhints]));
-	for (int i = 0; i < nhints; i++)
-		hintsREAL[i] = hints[nhints - i - 1];
-	row->nhints = nhints;
-	row->hints = hintsREAL;
-	row->content = content;
-
-	return row;
+	struct Hint *first = malloc (sizeof(struct Hint));
+	if (first == 0)
+		exit(1);
+	first->hint = hints[0];
+	struct Hint *this = first;
+	for (int i = 1; i < nhints; i++) {
+		struct Hint *next = malloc (sizeof(struct Hint));
+		if (next == 0)
+			exit (1);
+		next->hint = hints[i];
+		this->next = next;
+		this = next;
+	}
+	return first;
 }
 
-
-
-int main ()
+static void
+printhints(struct Hint *first)
 {
-	unsigned long long int rowcontent = binStoint("110010110");
-	struct Row *row = makerow(rowcontent);
-	for (int i = 0; i < row->nhints; i++)
-		printf("%d\n", *(row->hints + i));
-	printf("%d\n%lld\n", row->nhints, row->content);
-	return rowlength(rowcontent);
+	for (struct Hint *this = first;	this; this = this->next) {
+		printf("%d ", this->hint);
+	}
+	puts("\n");
 }
 
-//1 -> 1 current hint is incremented
-//1 -> 0 increment nhints
-//0 -> 1 nothing happens
-//0 -> 0 nothing happens
+
+int
+main ()
+{
+	unsigned long long int rowcontent = binStoint("110101");
+	struct Hint *hint = findrowhints(rowcontent);
+	printhints(hint);
+
+	return 0;
+}
