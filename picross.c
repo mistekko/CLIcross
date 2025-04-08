b195f8051f8aede21f9581a60fc472399b9febfb
mistekko <steamlie@protonmail.com>
2025-04-07 21:51:01 -0500

    Add level parsing function
    function `parselevel` now populates the game object with hints for rows read
    from a (currently hardcoded) file. This file's syntax is as follows:
    - each line must be the same length
    - the first and last lines and columns are read but discarded
    - `1' denotes a filled cell and `0' denotes an empty one
    - extra lines and columns (even just whitespace) WILL mess things up

--- picross.c
+++ picross.c
@@ -4,6 +4,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#define MAXROWS 100
 
 struct Hint;
 struct Hint {
@@ -27,9 +31,14 @@ static void parselevel(); // todo
 static inline int rowlength(unsigned long long int row);
 static struct Hint *findrowhints(unsigned long long int row);
 static void printhints(struct Hint *first);
-static char cellstatus(int x, int y); // todo: need Game object first
+static char cellstatus(int x, int y); // todo: need full Game object first
 static void markcell(int x, int y, int reject); /* reject: 0=fill, 1=reject */ //todo
-static void creategame(); // todo: returns fully populated Game struct
+static void parselevel();
+
+static struct Game game = {
+	.posx = 0,
+	.posy = 0
+};
 
 static unsigned long long int
 binStoint(const char *s)
@@ -49,7 +58,7 @@ rowlength(unsigned long long int row)
 	while (row >> length) {
 		length += 1;
 	}
-	return length - 1; /* first 1 allows for leading 0s in row content*/
+	return length - 1; /* first 1 allows for leading 0s in row content */
 }
 
 static struct Hint
@@ -68,12 +77,12 @@ static struct Hint
 		} else
 			chain = 0;
 	}
-	if (!nhints)
-		exit(1);
-
 	struct Hint *first = malloc (sizeof(struct Hint));
 	if (first == 0)
 		exit(1);
+	first->hint = 0;
+	if (!nhints)
+		return first;
 	first->hint = hints[0];
 	struct Hint *this = first;
 	for (int i = 1; i < nhints; i++) {
@@ -93,16 +102,62 @@ printhints(struct Hint *first)
 	for (struct Hint *this = first;	this; this = this->next) {
 		printf("%d ", this->hint);
 	}
-	puts("\n");
+	printf("\n");
 }
 
+static void
+parselevel(const char* levelpath)
+{
+	FILE *levelf = fopen(levelpath, "r");
+	if (!levelf) {
+		exit(1);
+	}
+	struct Hint *hints[MAXROWS];
+	char *buffer = NULL;
+	char *line = NULL;
+	ssize_t llength = 0;
+	size_t n = 0;
+	int nrows = 0;
+	getline(&buffer, &n, levelf);
+	for (; (llength = getline(&buffer, &n, levelf)) != -1; nrows++) {
+		if (nrows == 0) {
+			game.ncols = llength - 3;
+			line = malloc(sizeof(char *) * (llength - 3 + 1 + 1));
+			if (!line) {
+				exit(1);
+			}
+			line[0] = '1'; /* see rowlength() */
+			line++;
+		} else if (llength - 3 > game.ncols) {
+			printf("Warning: line %d is too long. "
+			       "Ignoring excess characters...\n",
+			       nrows + 1);
+		} else if (llength - 3 < game.ncols) {
+			printf("Error: line %d is too short (%ld). "
+			       "Exiting...\n",
+			       nrows + 1, llength);
+			exit(1);
+		}
+		strlcpy(line, buffer + 1, game.ncols + 1);
+		hints[nrows] = findrowhints(binStoint(line - 1));
+	}
+	free(buffer);
+	free(--line);
+	game.nrows = nrows;
+	game.rowhints = malloc(sizeof(void *) * nrows);
+	memcpy(game.rowhints, hints, sizeof(void *) * (nrows - 1));
+}
 
 int
 main ()
 {
-	unsigned long long int rowcontent = binStoint("110101");
-	struct Hint *hint = findrowhints(rowcontent);
-	printhints(hint);
+	parselevel("./level.pic");
+	printf("rows: %d\n", game.nrows);
+	printf("cols: %d\n", game.ncols);
 
+	for (int i = 0; i < game.nrows; i++) {
+		printf("%.2d: ", i + 1);
+		printhints(game.rowhints[i]);
+	}
 	return 0;
 }
