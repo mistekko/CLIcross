ce3e8fc2dedafa486f475562c4993a6162d08994
mistekko <steamlie@protonmail.com>
2025-05-05 13:27:59 -0500

    Clean up code
    Some code was moved, some comments added or removed or changed, and `strlcpy' replaced with `strncpy' for increased portability. The only functional addition is that now `parselevel()` populates game.colhints.

--- picross.c
+++ picross.c
@@ -1,13 +1,10 @@
-/* Local Variables: */
-/* compile-command: "cc picross.c -Wall -Wextra -pedantic -o p" */
-/* End: */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 
 #define MAXROWS 100
+
 typedef unsigned long long int Row;
 
 struct Hint;
@@ -21,23 +18,22 @@ struct Game {
 	int ncols;
 	struct Hint **rowhints;
 	struct Hint **colhints;
-	Row *level; /* all level data */
+	Row *level; /* level row data as parsed by parselevel */
 	Row *filledmasks;
-	Row *rejectedmasks; /* Row masks of crossed-out cell */
+	Row *rejectmasks; /* Row masks of crossed-out cell */
 	int posx, posy;
 };
 
 static inline Row binStoint(const char *s);
 static void inttobinS(Row r, char * buffer);
 
-static void parselevel(); // todo
 static inline int rowlength(Row r);
 static struct Hint *chainhints(int *hints, int nhints);
 static struct Hint *findrowhints(Row r);
 static struct Hint *findcolhints(Row *level, int col);
 static void printhints(struct Hint *first);
-static char cellstatus(int x, int y); // todo: need full Game object first
-static void markcell(int x, int y, int reject); /* reject: 0=fill, 1=reject */ //todo
+static char cellstatus(int x, int y); // todo: need full Game object first; initialise cells as zero; then we can start talking
+static void markcell(int x, int y, int reject); /* reject: 0=fill, 1=reject */ //todo; need cellstatus first!
 static void parselevel();
 
 static struct Game game = {
@@ -81,7 +77,7 @@ struct Hint *
 chainhints(int *hints, int nhints)
 {
 
-	struct Hint *first = malloc (sizeof(struct Hint));
+	struct Hint *first = malloc(sizeof(struct Hint));
 	if (!first)
 		exit(1);
 	first->hint = 0;
@@ -90,7 +86,7 @@ chainhints(int *hints, int nhints)
 	first->hint = hints[nhints - 1];
 	struct Hint *this = first;
 	for (int i = nhints - 2; i >= 0; i--) {
-		struct Hint *next = malloc (sizeof(struct Hint));
+		struct Hint *next = malloc(sizeof(struct Hint));
 		if (!next)
 			exit (1);
 		next->hint = hints[i];
@@ -118,9 +114,9 @@ findrowhints(Row r)
 }
 
 struct Hint *
-findcolwhints(Row *level, int col)
+findcolhints(Row *level, int col)
 {
-	int hints[game.nrows / 2 + 1];
+	int hints[game.nrows / 2 + 1]; /* max. possible */
 	int nhints = 0;
 	for (int i = 0, chain = 0; i < game.nrows; i++) {
 		if (level[i] >> (game.ncols - col) & 1) {
@@ -147,9 +143,8 @@ static void
 parselevel(const char* levelpath)
 {
 	FILE *levelf = fopen(levelpath, "r");
-	if (!levelf) {
+	if (!levelf)
 		exit(1);
-	}
 	struct Hint *hints[MAXROWS];
 	Row level[MAXROWS];
 	char *buffer = NULL;
@@ -160,12 +155,14 @@ parselevel(const char* levelpath)
 	getline(&buffer, &n, levelf);
 	for (; (llength = getline(&buffer, &n, levelf)) != -1; nrows++) {
 		if (nrows == 0) {
+			/* 3: 1st and last char (see level syntax) and \n */
 			game.ncols = llength - 3;
+			/* 1 + 1: leading '1' and terminating '\0'*/
 			line = malloc(sizeof(char *) * (llength - 3 + 1 + 1));
-			if (!line) {
+			line[(llength - 3 + 1 + 1) - 1] = '\0';
+			if (!line)
 				exit(1);
-			}
-			line[0] = '1'; /* see rowlength() */
+			line[0] = '1';
 			line++;
 		} else if (llength - 3 > game.ncols) {
 			printf("Warning: line %d is too long. "
@@ -177,17 +174,22 @@ parselevel(const char* levelpath)
 			       nrows + 1, llength);
 			exit(1);
 		}
-		strlcpy(line, buffer + 1, game.ncols + 1);
+		strncpy(line, buffer + 1, game.ncols);
 		hints[nrows] = findrowhints(binStoint(line - 1));
 		level[nrows] = binStoint(line - 1);
 	}
 	free(buffer);
 	free(--line);
-	game.nrows = --nrows; // last row has no level info
+	game.nrows = --nrows; /* ignore last row (see level syntax) */
 	game.rowhints = malloc(sizeof(void *) * nrows);
 	memcpy(game.rowhints, hints, sizeof(void *) * nrows);
 	game.level = malloc(sizeof(Row) * nrows);
 	memcpy(game.level, level, sizeof(void *) * nrows);
+	game.colhints = malloc(sizeof(void *) * game.ncols);
+	for (int i = 0; i < game.ncols; i++)
+		game.colhints[i] = findcolhints(game.level, i+1);
+	game.filledmasks = malloc(sizeof(Row) * nrows);
+	game.rejectmasks = malloc(sizeof(Row) * nrows);
 }
 
 int
@@ -200,6 +202,11 @@ main ()
 	for (int i = 0; i < game.nrows; i++) {
 		printf("%.2d: ", i + 1);
 		printhints(game.rowhints[i]);
+	}
+
+	for (int i = 0; i < game.ncols; i++) {
+		printf("%.2d: ", i + 1);
+		printhints(game.colhints[i]);
 	}
 
 	char *buffer = malloc(sizeof(char) * game.ncols + 1);
